08c3ec66bedd985724273ec1406ebe18d67a3c68
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2023-09-07 19:33:35 +0700

    Pertemuan4-PPW2-Materi-Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,5 @@ Route::get('/about', function () {
         "email" => "prasetyoedi@gmail.com",
     ]);
 });
+
+Route::get('/boom', [PostController::class, 'boomsport']);
